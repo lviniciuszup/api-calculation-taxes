e50644224a64fdcdc5707a11d95b5e818c07d0e7
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-07 08:26:52 -0300

    feat: add model User

--- src/main/java/com/calctax/tax_calculation_api/models/User.java
+++ src/main/java/com/calctax/tax_calculation_api/models/User.java
@@ -1,4 +1,31 @@
 package com.calctax.tax_calculation_api.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name="users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name="role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
+
 }
