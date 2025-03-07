8551f587858f63dc208145fba9ce852baf9410f5
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-07 10:24:52 -0300

    feat: add dto class for Tax model

--- src/main/java/com/calctax/tax_calculation_api/dtos/TaxDTO.java
+++ src/main/java/com/calctax/tax_calculation_api/dtos/TaxDTO.java
@@ -1,4 +1,22 @@
 package com.calctax.tax_calculation_api.dtos;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class TaxDTO {
+
+    @NotEmpty(message = "Name cannot be empty")
+    private String name;
+
+    @NotEmpty(message = "Description cannot be empty")
+    private String description;
+
+    @NotNull(message = "Aliquota cannot be null")
+    @DecimalMin(value = "0.0", inclusive = false, message = "A Alíquota precisa ser maior que zero")
+    private BigDecimal aliquot;
 }
