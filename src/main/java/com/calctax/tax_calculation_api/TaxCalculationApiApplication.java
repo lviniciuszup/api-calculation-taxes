2fc6e2dcd3b9fb92ee61cf315f720064ee113190
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-07 06:54:43 -0300

    chore: Spring Project with Java, Maven, JWT and other dependencies

---
+++ src/main/java/com/calctax/tax_calculation_api/TaxCalculationApiApplication.java
@@ -0,0 +1,13 @@
+package com.calctax.tax_calculation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxCalculationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxCalculationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calctax/tax_calculation_api/TaxCalculationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calctax.tax_calculation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxCalculationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
