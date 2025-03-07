43238a79c2d9ee3d036c64dea271039374d7cc58
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-07 10:25:29 -0300

    feat: add dto class RegisterUserDTO

--- src/main/java/com/calctax/tax_calculation_api/dtos/RegisterUserDTO.java
+++ src/main/java/com/calctax/tax_calculation_api/dtos/RegisterUserDTO.java
@@ -1,14 +1,18 @@
 package com.calctax.tax_calculation_api.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import java.math.BigDecimal;
 import java.util.Set;
 
 @Data
-public class UserDTO {
-    private String name;
+public class RegisterUserDTO {
+    @NotEmpty(message = "O usuário não pode ser vázio")
+    @Size(min = 3, max = 20, message = "Por favor, o nome de usuário precisa ter entre 3 e 20 caracteres" )
     private String username;
+    @NotEmpty(message = "A senha não pode ser vázia")
+    @Size(min = 6, message = "Por favor, a senha precisa ter no mínimo 6 caracteres" )
     private String password;
     private Set<Roles> roles;
 }
