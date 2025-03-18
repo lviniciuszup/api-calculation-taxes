164e380eccfe61fc037f946d9400e20756ecfe38
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-18 13:50:53 -0300

    feat: rename description to CalculatedTaxDTO and remove unnecessary field

--- src/main/java/com/calctax/tax_calculation_api/dtos/CalculatedTaxDTO.java
+++ src/main/java/com/calctax/tax_calculation_api/dtos/CalculatedTaxDTO.java
@@ -1,17 +1,12 @@
 package com.calctax.tax_calculation_api.dtos;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
 import java.math.BigDecimal;
 
 @Data
-public class TaxDTO {
+public class CalculatedTaxDTO {
 
     private String name;
-    private String description;
     private BigDecimal baseValue;
     private BigDecimal aliquot;
     private BigDecimal calculatedValue;
