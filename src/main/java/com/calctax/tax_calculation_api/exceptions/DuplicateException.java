1db5a87a0cc98e3f5b637efcb2225b77e03636d1
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-18 13:25:09 -0300

    feat: create DuplicateException class for handling duplicate data errors

--- src/main/java/com/calctax/tax_calculation_api/exceptions/DuplicateException.java
+++ src/main/java/com/calctax/tax_calculation_api/exceptions/DuplicateException.java
@@ -1,7 +1,7 @@
 package com.calctax.tax_calculation_api.exceptions;
 
 public class DuplicateException extends RuntimeException {
-  public DuplicateException(String message) {
-    super(message);
-  }
+    public DuplicateException(String message) {
+        super(message);
+    }
 }
