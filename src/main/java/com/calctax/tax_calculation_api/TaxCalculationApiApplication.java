ba5506d029fc05c23700a5a04fc863a0107160d8
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-18 16:56:00 -0300

    chore: configure dotenv for environment variables support

--- src/main/java/com/calctax/tax_calculation_api/TaxCalculationApiApplication.java
+++ src/main/java/com/calctax/tax_calculation_api/TaxCalculationApiApplication.java
@@ -1,5 +1,6 @@
 package com.calctax.tax_calculation_api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaxCalculationApiApplication {
 
 	public static void main(String[] args) {
+
+			Dotenv dotenv = Dotenv.configure().load();
+
+			String dbUrl = dotenv.get("DB_URL");
+			String dbUsername = dotenv.get("DB_USERNAME");
+			String dbPassword = dotenv.get("DB_PASSWORD");
+
+
+			System.setProperty("DB_URL", dbUrl);
+			System.setProperty("DB_USERNAME", dbUsername);
+			System.setProperty("DB_PASSWORD", dbPassword);
+
 		SpringApplication.run(TaxCalculationApiApplication.class, args);
 	}
 
