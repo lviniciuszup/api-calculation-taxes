5ee15abd08f2f847cba96c21f36fdd29afbe61e5
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-18 13:25:52 -0300

    feat: add GeneralExceptionHandler class for handling global exceptions

--- src/main/java/com/calctax/tax_calculation_api/exceptions/GeneralExceptionHandler.java
+++ src/main/java/com/calctax/tax_calculation_api/exceptions/GeneralExceptionHandler.java
@@ -1,4 +1,44 @@
 package com.calctax.tax_calculation_api.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class GeneralExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>>
+    handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()){
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+    @ExceptionHandler (DuplicateException.class)
+    public ResponseEntity<String> handleDuplicateException(DuplicateException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+    @ExceptionHandler (NotFoundException.class)
+    public ResponseEntity<String> handleDuplicateException(NotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler (InvalidException.class)
+    public ResponseEntity<String> handleInvalidException(InvalidException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleInvalidRoleException(HttpMessageNotReadableException ex) {
+        return new ResponseEntity<>("Por favor a role precisa ser informada como: ADMIN ou USER.", HttpStatus.BAD_REQUEST);
+    }
 }
