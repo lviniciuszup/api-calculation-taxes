f4bf7a632ed5cba3bef51d04e71e5a45b59a9836
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-18 13:22:09 -0300

    feat: add RoleRepository interface for role persistence operations

--- src/main/java/com/calctax/tax_calculation_api/repositories/RoleRepository.java
+++ src/main/java/com/calctax/tax_calculation_api/repositories/RoleRepository.java
@@ -1,9 +1,11 @@
 package com.calctax.tax_calculation_api.repositories;
 
+import com.calctax.tax_calculation_api.dtos.RoleName;
 import com.calctax.tax_calculation_api.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+   Optional<Role>findByName(RoleName name);
 }
