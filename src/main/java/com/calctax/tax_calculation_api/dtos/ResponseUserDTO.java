a5a20aa2fbff244f6bf89a95abd40eceec780cce
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-18 13:32:20 -0300

    feat: update ResponseUserDTO by replacing password with id and adding roles set

--- src/main/java/com/calctax/tax_calculation_api/dtos/ResponseUserDTO.java
+++ src/main/java/com/calctax/tax_calculation_api/dtos/ResponseUserDTO.java
@@ -1,12 +1,15 @@
 package com.calctax.tax_calculation_api.dtos;
 
+import com.calctax.tax_calculation_api.models.Role;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Set;
 
 @Data
+@AllArgsConstructor
 public class ResponseUserDTO {
-
+    private Long id;
     private String username;
-    private String password;
+    private Set<Role> roles;
 }
