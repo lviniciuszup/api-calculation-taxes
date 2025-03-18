83c55063ac4f850fcdf6e1ed1ebe93269edb0c32
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-18 13:21:03 -0300

    feat: add TaxServices interface for tax-related business logic

--- src/main/java/com/calctax/tax_calculation_api/services/TaxServices.java
+++ src/main/java/com/calctax/tax_calculation_api/services/TaxServices.java
@@ -1,16 +1,13 @@
 package com.calctax.tax_calculation_api.services;
 
-import com.calctax.tax_calculation_api.dtos.LoginUserDTO;
-import com.calctax.tax_calculation_api.dtos.RegisterUserDTO;
-import com.calctax.tax_calculation_api.dtos.ResponseUserDTO;
-import com.calctax.tax_calculation_api.models.User;
-import org.apache.coyote.Response;
+import com.calctax.tax_calculation_api.dtos.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public interface UserServices {
-    ResponseUserDTO registerUser(RegisterUserDTO registerUserDTO);
-    String loginUser(LoginUserDTO loginUserDTO);
+public interface TaxServices {
+    ResponseTaxDTO registerTax(RegisterTaxDTO registerTaxDTO);
+    ResponseTaxDTO getTaxById(Long id);
+    CalculatedTaxDTO calculatedTax(ResponseTaxDTO responseTaxDTO);
+    List<ResponseTaxDTO> getAllTaxes();
+    void deleteTaxById(Long id);
 }
