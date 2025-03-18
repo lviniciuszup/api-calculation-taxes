55c290e746c88fdb4b827eb418ce2e02f2430d8a
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-18 16:51:19 -0300

    refactor: update SecurityConfig with improved authorization rules

--- src/main/java/com/calctax/tax_calculation_api/infra/SecurityConfig.java
+++ src/main/java/com/calctax/tax_calculation_api/infra/SecurityConfig.java
@@ -1,32 +1,38 @@
-package com.calctax.tax_calculation_api.infra.security;
+package com.calctax.tax_calculation_api.infra;
 
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import br.com.zup.impostos.infra.JwtAuthenticationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Autowired
-    private  JwtAuthenticationFilter jwtAuthenticationFilter;
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/user/register", "/api/user/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/tax/tipos", "/tax/calculo").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/tax/tipos", "/tax/tipos/{id}").authenticated()
+                        .requestMatchers(HttpMethod.DELETE,"/tax/tipos/{id}").hasAuthority("ADMIN").anyRequest().authenticated()
                 );
         http.csrf(AbstractHttpConfigurer::disable);
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
