23ae1dda1dbe8297975aabc3133680f4db95d5e6
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-18 13:34:40 -0300

    feat: update RegisterUserDTO to include necessary fields for user registration

--- src/main/java/com/calctax/tax_calculation_api/dtos/RegisterUserDTO.java
+++ src/main/java/com/calctax/tax_calculation_api/dtos/RegisterUserDTO.java
@@ -2,11 +2,13 @@ package com.calctax.tax_calculation_api.dtos;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Set;
 
 @Data
+@AllArgsConstructor
 public class RegisterUserDTO {
     @NotEmpty(message = "O usuário não pode ser vázio")
     @Size(min = 3, max = 20, message = "Por favor, o nome de usuário precisa ter entre 3 e 20 caracteres" )
@@ -14,5 +16,5 @@ public class RegisterUserDTO {
     @NotEmpty(message = "A senha não pode ser vázia")
     @Size(min = 6, message = "Por favor, a senha precisa ter no mínimo 6 caracteres" )
     private String password;
-    private Set<Roles> roles;
+    private Set<String> roles;
 }
