ea84598075a1e729da30694368ddd465c20d1ebe
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-18 13:25:22 -0300

    feat: create InvalidException class for handling invalid data errors

--- src/main/java/com/calctax/tax_calculation_api/exceptions/InvalidException.java
+++ src/main/java/com/calctax/tax_calculation_api/exceptions/InvalidException.java
@@ -1,7 +1,7 @@
 package com.calctax.tax_calculation_api.exceptions;
 
 public class InvalidException extends RuntimeException {
-  public InvalidException(String message) {
-    super(message);
-  }
+    public InvalidException(String message) {
+        super(message);
+    }
 }
