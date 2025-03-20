ad117e6618bccec22dba5a0c11b0cdd7b97b3872
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-20 17:28:08 -0300

    refactor: update RoleRepository logic

--- src/main/java/com/calctax/tax_calculation_api/repositories/RoleRepository.java
+++ src/main/java/com/calctax/tax_calculation_api/repositories/RoleRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-   Optional<Role>findByName(RoleName name);
+   Optional<Role>findByName(String name);
+   boolean existsByName(String name);
+
 }
