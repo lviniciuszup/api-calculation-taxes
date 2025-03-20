04e5398d4fd5fcd62aa211d9fbfed6c03f1fa624
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-20 17:23:21 -0300

    Create construtor for DTO

--- src/main/java/com/calctax/tax_calculation_api/dtos/CalculatedTaxDTO.java
+++ src/main/java/com/calctax/tax_calculation_api/dtos/CalculatedTaxDTO.java
@@ -1,9 +1,14 @@
 package com.calctax.tax_calculation_api.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CalculatedTaxDTO {
 
     private String name;
