2d3950ac0b855fa40c0f01c3d2a6e080ea57dbc4
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-21 08:53:11 -0300

    Remove Lombok and manually implement getters, setters, and constructor in RegisterTaxDTO for JaCoCo compatibility

--- src/main/java/com/calctax/tax_calculation_api/dtos/RegisterTaxDTO.java
+++ src/main/java/com/calctax/tax_calculation_api/dtos/RegisterTaxDTO.java
@@ -3,13 +3,9 @@ package com.calctax.tax_calculation_api.dtos;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 import java.math.BigDecimal;
 
-@Data
-@AllArgsConstructor
 public class RegisterTaxDTO {
 
     @NotEmpty(message = "Nome não pode ser vázio")
@@ -19,4 +15,34 @@ public class RegisterTaxDTO {
     @NotNull(message = "Aliquota não pode ser nulo")
     @DecimalMin(value = "0.0", inclusive = false, message = "A Alíquota precisa ser maior que zero")
     private BigDecimal aliquot;
+
+    public RegisterTaxDTO(String name, String description, BigDecimal aliquot) {
+        this.name = name;
+        this.description = description;
+        this.aliquot = aliquot;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAliquot() {
+        return aliquot;
+    }
+
+    public void setAliquot(BigDecimal aliquot) {
+        this.aliquot = aliquot;
+    }
 }
