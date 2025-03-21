6be242bc0122e9029600fd065148d549f5fc7869
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-21 08:59:44 -0300

    Remove Lombok and manually implement getters, setters, and constructors in DTOs and Models for JaCoCo compatibility

--- src/main/java/com/calctax/tax_calculation_api/dtos/ResponseTaxDTO.java
+++ src/main/java/com/calctax/tax_calculation_api/dtos/ResponseTaxDTO.java
@@ -6,10 +6,48 @@ import lombok.Data;
 import java.math.BigDecimal;
 
 @Data
-@AllArgsConstructor
 public class ResponseTaxDTO {
     private Long taxId;
     private String name;
     private String description;
     private BigDecimal aliquot;
+
+    public ResponseTaxDTO(Long taxId, String name, String description, BigDecimal aliquot) {
+        this.taxId = taxId;
+        this.name = name;
+        this.description = description;
+        this.aliquot = aliquot;
+    }
+
+    public Long getTaxId() {
+        return taxId;
+    }
+
+    public void setTaxId(Long taxId) {
+        this.taxId = taxId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAliquot() {
+        return aliquot;
+    }
+
+    public void setAliquot(BigDecimal aliquot) {
+        this.aliquot = aliquot;
+    }
 }
