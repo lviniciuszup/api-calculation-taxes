057c633587176119b6bff32cd16a4154cfe4f77e
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-21 08:53:53 -0300

    Remove Lombok and manually implement getters, setters, and constructor in RegisterUserDTO for JaCoCo compatibility

--- src/main/java/com/calctax/tax_calculation_api/dtos/RegisterUserDTO.java
+++ src/main/java/com/calctax/tax_calculation_api/dtos/RegisterUserDTO.java
@@ -8,9 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class RegisterUserDTO {
     @NotEmpty(message = "O usuário não pode ser vázio")
     @Size(min = 3, max = 20, message = "Por favor, o nome de usuário precisa ter entre 3 e 20 caracteres" )
@@ -19,4 +17,38 @@ public class RegisterUserDTO {
     @Size(min = 6, message = "Por favor, a senha precisa ter no mínimo 6 caracteres" )
     private String password;
     private Set<String> roles;
+
+
+    public RegisterUserDTO(String username, String password, Set<String>roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+
+    }
+
+    public RegisterUserDTO() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
 }
