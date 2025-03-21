2ab015019f187e5703c6a5c3a5717c42a8600732
lviniciuszup <luiz.vincius@zup.com.br>
2025-03-21 08:52:09 -0300

    Remove Lombok and manually implement getters, setters, and constructor in LoginUserDTO for JaCoCo compatibility

--- src/main/java/com/calctax/tax_calculation_api/dtos/LoginUserDTO.java
+++ src/main/java/com/calctax/tax_calculation_api/dtos/LoginUserDTO.java
@@ -5,9 +5,30 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class LoginUserDTO {
     private String username;
     private String password;
+
+    public LoginUserDTO(String password, String username) {
+        this.password = password;
+        this.username = username;
+    }
+
+    public LoginUserDTO() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
